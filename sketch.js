2f3bce3c8b244d6aaeef87e604baa4d691eadace
Rayan-Tarapore <ratitarapore@gmail.com>
2020-12-22 21:34:16 +0530

    CHECK THE ERRORS

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
+var grd1,grd2,grd3,grd4
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(800,480);
+  
+  grd1 = new Ground(displayWidth/2,displayHeight-20,displayWidth,40);
 }
 
 function draw() {
-  background(255,255,255);  
+  background(200,200,200);  
   drawSprites();
 }
